6e9d42dd2c53cb1934b492c77b03b7b8e173120f
fersonull <culaspero@gmail.com>
2025-12-23 01:25:09 +0800

    add app navs and font assets

---
+++ src/navigations/app-navigator.jsx
@@ -0,0 +1,14 @@
+import { NavigationContainer } from "@react-navigation/native"
+import { createNativeStackNavigator } from "@react-navigation/native-stack"
+
+const Stack = createNativeStackNavigator()
+
+export default function AppNavigator() {
+    return (
+        <NavigationContainer>
+            <Stack.Navigator>
+                <Stack.Screen name='OnBoarding' />
+            </Stack.Navigator>
+        </NavigationContainer>
+    )
+}
